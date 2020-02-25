cc5d51cb9d08d122d0f5bf1c5f54d95ed6ed652c
Simon Westman <simonwestman88@gmail.com>
2020-02-25 10:52:15 +0100

    Added UnitTest-Assembly, and added if-statement in AddVehicle(regNum, spot) for tidying up IndexOutOfRangeException.

---
+++ AssemblyTwoTest/ParkingLotTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssemblyOne;
+using AssemblyTwo;
+
+namespace AssemblyTwoTest
+{
+    [TestClass]
+    public class ParkingLotTest
+    {
+        [TestMethod]
+        public void AddVehicleValidReg()
+        {
+            Car testCar = new Car("Legit222");
+            ParkingLot testLot = new ParkingLot(10, 2);
+            int returnValue = testLot.AddVehicle(testCar);
+            Assert.AreEqual<int>(1, returnValue);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddVehicleRegNumIsNullOrEmpty()
+        {
+            Car testCar = new Car("");
+            ParkingLot testLot = new ParkingLot(10, 2);
+            testLot.AddVehicle(testCar);
+        }
+
+        [TestMethod]
+        public void AddVehicleInvalidSpot()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void AddVehicleValidSpot()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
